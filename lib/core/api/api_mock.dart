924e2c1107dc0e688f31e81863c2c7a2a7188f0d
SEINI SALIO <sseini@bceao.int>
2025-02-26 10:11:33 +0000

    First ...

--- lib/core/api/api_mock.dart
+++ lib/core/api/api_mock.dart
@@ -1,28 +1,11 @@
 // coverage:ignore-file
-
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/services.dart';
 
 import 'api_mock_resolver/auth/mock_login.dart';
 
 /// Simple API Mock interceptor
 /// Only for demo purposes
 class MockInterceptor extends Interceptor {
-  //
-  static const _jsonDir = 'assets/json/';
-
-  static const String httpGet = 'GET';
-  static const String httpPost = 'POST';
-  static const String httpDelete = 'DELETE';
-  static const String httpPut = 'PUT';
-
-  Future<dynamic> retrieveData(String resourcePath) async {
-    final String data = await rootBundle.loadString(_jsonDir + resourcePath);
-    final map = jsonDecode(data);
-    return map;
-  }
 
   @override
   void onRequest(
